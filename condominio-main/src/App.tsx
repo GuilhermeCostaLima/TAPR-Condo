96d77cfb2590df962f57861388583d3706c2df00
Guilherme da Costa de Lima <127263348+GuilhermeCostaLima@users.noreply.github.com>
2025-11-13 21:45:55 -0300

    Add route to Auth component

--- condominio-main/src/App.tsx
+++ condominio-main/src/App.tsx
@@ -28,6 +28,7 @@ const App = () => (
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/auth" element={<Auth />} />
+            <Route path="*" element={<Auth />} />
           </Routes>
         </BrowserRouter>
       </TooltipProvider>
